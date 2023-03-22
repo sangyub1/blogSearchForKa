41df558a55cb96fefb217f73eee2c10d1a548849
SangYub <goodtkdduq1@gmail.com>
2023-03-22 10:17:15 +0900

    swagger 추가

--- src/main/java/blog/search/blogsearch/common/controller/BlogSearchController.java
+++ src/main/java/blog/search/blogsearch/common/controller/BlogSearchController.java
@@ -5,6 +5,7 @@ import blog.search.blogsearch.common.dto.SearchResponseDto;
 import blog.search.blogsearch.common.service.SearchService;
 import blog.search.blogsearch.interest.dto.InterestSearchDto;
 import blog.search.blogsearch.interest.service.InterestRegService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +18,14 @@ import java.util.List;
 public class BlogSearchController {
     private final SearchService searchService;
 
-    private final InterestRegService interestRegService;
-
     @GetMapping("/search")
+    @ApiOperation(value = "블로그 검색")
     public SearchResponseDto search(@RequestBody @Valid RequestDto requestDto){
         return searchService.searchBlog(requestDto);
     }
+
     @GetMapping("/search-list")
+    @ApiOperation(value = "블로그 검색 인기 순위")
     public List<InterestSearchDto> interestSearchList(){
         return searchService.getInterestSearch();
     }
